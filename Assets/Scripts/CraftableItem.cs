0651a9c75a8780ca9a576f0f99386b7cfdc1fbb5
lucal <louwril@hva.nl>
2019-10-16 15:17:25 +0200

    First Commit, hiervoor geen remote.

---
+++ Assets/Scripts/CraftableItem.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CraftableItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject thisItem;
+    public GameObject[] requiredItem;
+    
+     
+    private bool hovered;
+    private GameObject player;
+    private GameObject itemManager;
+
+
+    public void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        itemManager = GameObject.FindGameObjectWithTag("ItemManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hovered)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //perform things, check for required items
+                CheckForItems();
+            }
+        }
+    }
+
+    public void CheckForItems()
+    {
+        int itemsInManager = itemManager.transform.childCount;
+
+        if(itemsInManager > 0)
+        {
+            Debug.Log("Start Check");
+            int itemsFound = 0;
+
+            for (int i = 0; i < itemsInManager; i++)
+            {
+                for (int j = 0; j < requiredItem.Length; j++)
+                {
+                    Debug.Log(itemManager.transform.GetChild(i).name);
+                    if (itemManager.transform.GetChild(i).name == requiredItem[j].name)
+                    {
+                        itemsFound++;
+                        break;
+                    }
+                }
+            }
+            Debug.Log("Eind Check");
+            if (itemsFound >= requiredItem.Length)
+            {
+                
+
+                Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y + 2, player.transform.position.z);
+                GameObject spawnedItem = Instantiate(thisItem, playerPos, Quaternion.identity);
+                player.GetComponent<PlayerInventory>().AddItem(spawnedItem);
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hovered = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hovered = false;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Variables
+    public float maxHealth, maxThirst, maxHunger;
+    public float thirstIncreaseRate, hungerIncreaseRate;
+    [HideInInspector] public float health, thirst, hunger;
+
+    public Image fillerHealth, fillerHunger, fillerThirst;
+
+    public bool playerDead;
+    public bool weaponEquipped;
+    public float damage;
+    public static bool triggeringWithAI;
+    public static GameObject aiObject;
+
+    public static bool triggeringWithTree;
+    public static GameObject treeObject;
+
+
+    // Functions
+    public void Start()
+    {
+        health = maxHealth;
+        thirst = maxThirst;
+        hunger = maxHunger;
+
+        weaponEquipped = false;
+    }
+
+    public void FixedUpdate()
+    {
+        // increase thirst and hunger
+        if (!playerDead)
+        {
+            if(thirst >= 0)
+                thirst -= thirstIncreaseRate * Time.deltaTime;
+            if(hunger >= 0)
+                hunger -= hungerIncreaseRate * Time.deltaTime;
+        }
+
+        // check if thirst or hunger are passed max
+        // if so, decreases health
+        if (thirst <= 0 && !playerDead)
+        {
+            health -= 1 * Time.deltaTime;
+
+            //Debug.Log("You are dieing of thirst");
+        }
+        if (hunger <= 0 && !playerDead)
+        {
+            health -= 1 * Time.deltaTime;
+            //Debug.Log("You are dieing of hunger");
+        }
+
+        // Check if health reached 0
+        if (health <= 0)
+        {
+            Die();
+            Time.timeScale = 0;
+        }
+
+        // Tree Chopping
+        if (triggeringWithTree && treeObject)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack(treeObject);
+            }
+        }
+
+        // Collision AI
+        if (triggeringWithAI && aiObject)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack(aiObject);
+            }
+        }
+
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        fillerHealth.fillAmount = (health / maxHealth);
+        fillerThirst.fillAmount = (thirst / maxThirst);
+        fillerHunger.fillAmount = (hunger / maxHunger);
+    }
+    public void Die()
+    {
+        playerDead = true;
+        Debug.Log("You have died because of thirst or hunger");
+        //Time.timeScale = 0f;
+    }
+
+    public void Drink(float decreaseRate)
+    {
+        thirst += decreaseRate;
+    }
+    public void Eat(float decreaseRate)
+    {
+        hunger += decreaseRate;
+    }
+
+    public void Heal(float decreaseRate)
+    {
+        health += decreaseRate;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Animal")
+        {
+            aiObject = other.gameObject;
+            triggeringWithAI = true;
+        }
+        if (other.tag == "Tree")
+        {
+            triggeringWithTree = true;
+            treeObject = other.gameObject;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Animal")
+        {
+            aiObject = null;
+            triggeringWithAI = false;
+        }
+        if (other.tag == "Tree")
+        {
+            triggeringWithTree = false;
+            treeObject = null;
+        }
+    }
+
+    public void Attack(GameObject target)
+    {
+        if (target.tag == "Animal" && weaponEquipped)
+        {
+            Animal animal = target.GetComponent<Animal>();
+            animal.health -= damage;
+        }
+        if (target.tag == "Tree" && weaponEquipped)
+        {
+            Tree tree = target.GetComponent<Tree>();
+            tree.health -= damage;
+        }
+    }
+}
