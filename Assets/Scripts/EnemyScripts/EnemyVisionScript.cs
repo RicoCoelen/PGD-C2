e66e9ca71b48c53f769340d8533a05aabd2aeecc
rico-coelen <rico.coelen@gmail.com>
2019-11-19 21:27:48 +0100

    made enemy shoot, added bullet prefab, and added quite alot of fixes to enemy (hopefully)

--- Assets/Scripts/EnemyScripts/EnemyVisionScript.cs
+++ Assets/Scripts/EnemyScripts/EnemyVisionScript.cs
@@ -8,8 +8,6 @@ public class EnemyVisionScript : MonoBehaviour
     public float DetectionRange;
     public float lastDetectionTime;
     public float FOV = 90;
-
-    public float seeDistance;
     public GameObject PlayerGO;
     public GameObject currentTarget;
  
@@ -20,35 +18,33 @@ public class EnemyVisionScript : MonoBehaviour
 
     void Update()
     {
-        // get direction to player
-        Vector3 rayDirection = PlayerGO.transform.position - transform.position;
-
-        // raycast to player
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection, rayDirection.magnitude);
-        if (hit)
-        {
-            if (hit.collider.tag == "Player")
-            {
-                //Checks if the player is in the FOV
-                float angle = Vector2.Angle(rayDirection, Vector3.forward);
-                Debug.Log(angle);
-                if (angle < FOV / 2)
-                {
-                    Debug.DrawLine(transform.position, hit.point);
-                    GetComponentInParent<EnemyMainScript>().currentTarget = PlayerGO;
-                    currentTarget = PlayerGO;
-                    lastDetectionTime = Time.time;
-                } 
-            }
-        }
-
         // get distance
         float dis = Vector3.Distance(transform.position, PlayerGO.transform.position);
 
         // check if in range
         if (dis < DetectionRange)
         {
-            currentTarget = PlayerGO;
+            // get direction to player
+            Vector3 rayDirection = PlayerGO.transform.position - transform.position;
+
+            // raycast to player
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection, rayDirection.magnitude);
+            if (hit)
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    //Checks if the player is in the FOV
+                    float angle = Vector2.Angle(rayDirection, Vector3.forward);
+           
+                    if (angle < FOV / 2)
+                    {
+                        Debug.DrawLine(transform.position, hit.point);
+                        GetComponentInParent<EnemyMainScript>().currentTarget = PlayerGO;
+                        currentTarget = PlayerGO;
+                        lastDetectionTime = Time.time;
+                    } 
+                }
+            }
         }
 
         // make enemy forget after while
@@ -66,13 +62,13 @@ public class EnemyVisionScript : MonoBehaviour
 
         if (GetComponentInParent<EnemyMainScript>().facingRight == true)
         {
-            Gizmos.DrawLine(transform.position, transform.position + rot * Vector3.right * seeDistance);
-            Gizmos.DrawLine(transform.position, transform.position + rot2 * Vector3.right * seeDistance);
+            Gizmos.DrawLine(transform.position, transform.position + rot * Vector3.right);
+            Gizmos.DrawLine(transform.position, transform.position + rot2 * Vector3.right);
         }
         else
         {
-            Gizmos.DrawLine(transform.position, transform.position - rot * Vector2.right * seeDistance);
-            Gizmos.DrawLine(transform.position, transform.position - rot2 * Vector2.right * seeDistance);
+            Gizmos.DrawLine(transform.position, transform.position - rot * Vector2.right);
+            Gizmos.DrawLine(transform.position, transform.position - rot2 * Vector2.right);
         }
     } 
 }
