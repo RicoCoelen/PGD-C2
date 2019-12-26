cf40032f72edc9fbab6a233d486fc3d91fe44933
Casper <casperarends1910@gmail.com>
2019-12-26 21:13:32 +0100

    Door switches collision toegevoegd

---
+++ Assets/Scripts/World/Interactable objects/ButtonSwitches.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSwitches : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject switchOn, switchOff;
+    public GameObject player;
+    
+    public bool isOn = false;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        gameObject.GetComponent<SpriteRenderer>().sprite = switchOff.GetComponent<SpriteRenderer>().sprite;
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("collision button");
+        }
+
+        gameObject.GetComponent<SpriteRenderer>().sprite = switchOn.GetComponent<SpriteRenderer>().sprite;
+        isOn = true;
+
+    }
+}
---
+++ Assets/Scripts/World/SwitchManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] switches;
+
+    [SerializeField]
+    GameObject exitDoor;
+
+    [SerializeField]
+    Text switchCounter;
+
+    int noOfswitches = 0;
+
+    private void Start()
+    {
+        GetNoOfSwitches();
+    }
+
+    public int GetNoOfSwitches()
+    {
+
+        int x = 0;
+
+        for (int i = 0; i < switches.Length; i++)
+        {
+            if(switches[i].GetComponent<ButtonSwitches>().isOn == false)
+            {
+                x++;
+            }
+            else if (switches[i].GetComponent<ButtonSwitches>().isOn == true)
+            {
+                noOfswitches--;
+            }
+
+        }
+
+        noOfswitches = x;
+
+        return noOfswitches;
+    }
+
+    public void DoorState()
+    {
+
+    }
+}
