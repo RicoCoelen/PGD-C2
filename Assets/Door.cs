354183752cfd215c40a99e697d95c4b62c1c934f
Casper <casperarends1910@gmail.com>
2020-01-13 16:07:02 +0100

    RuleTilemap added + other tilemap features

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -21,25 +21,6 @@ public class Door : MonoBehaviour
         }
     }
 
-    public void openDoor()
-    {
-        
-        if (isopen == false)
-        {
-            gameObject.GetComponent<SpriteRenderer>().sprite = doorOpen.GetComponent<SpriteRenderer>().sprite;
-            gameObject.GetComponent<BoxCollider2D>().enabled = false;
-            isopen = true;
-        }
-
-        if (isopen == true)
-        {
-            gameObject.GetComponent<SpriteRenderer>().sprite = doorClosed.GetComponent<SpriteRenderer>().sprite;
-            gameObject.GetComponent<BoxCollider2D>().enabled = true;
-            isopen = false;
-        }
-        Debug.Log(isopen + "isopen");
-        
-    }
     // Update is called once per frame
     void Update()
     {
